d6722c6ca639ea978332a3c6b85c093a5bffd582
leekiwon31221213 <129711547+leekiwon31221213@users.noreply.github.com>
2024-01-30 18:58:12 +0900

    [update] 01-30

--- js/common.js
+++ js/common.js
@@ -2,41 +2,53 @@ let currentSection = 0;
 const sections = document.querySelectorAll('.section');
 const section3 = document.querySelector('.section3');
 let scrollStart = 0;
+let isTransitioning = false;
 
 const changeSection = (next) => {
-    if (next >= 0 && next < sections.length) {
-        sections[currentSection].style.left = '-100%'; // 현재 섹션을 왼쪽으로 이동
-        sections[next].style.left = '0'; // 다음 섹션을 화면표시
-        currentSection = next;
+  if (next >= 0 && next < sections.length && !isTransitioning) {
+    isTransitioning = true;
+
+    if ((currentSection === 0 && next === 1) || (currentSection === 1 && next === 0)) {
+      sections[currentSection].style.left = next > currentSection ? '-100%' : '100%';
+      sections[next].style.left = '0';
+    } else {
+      sections[currentSection].style.top = next > currentSection ? '-100%' : '100%';
+      sections[next].style.top = '0';
     }
 
-    document.body.style.overflow = currentSection === 2 ? 'auto' : 'hidden';
+    currentSection = next;
+
+    setTimeout(() => {
+      isTransitioning = false;
+    }, 500);
+  }
+
+  document.body.style.overflow = currentSection === 2 ? 'auto' : 'hidden';
 };
 
 window.addEventListener('wheel', (e) => {
-    if (currentSection < 2) {
-        if (e.deltaY > 0 && currentSection < sections.length - 1) {
-            changeSection(currentSection + 1);
-        } else if (e.deltaY < 0 && currentSection > 0) {
-            changeSection(currentSection - 1);
-        }
-    } else if (currentSection === 2) {
-        const isScrolledToTop = section3.scrollTop === 0;
-        if (e.deltaY < 0 && isScrolledToTop) {
-            if (scrollStart === 0) {
-                // 섹션 3에서 스크롤이 맨 위에 있을 때 스크롤 시작 지점을 기록
-                scrollStart = e.timeStamp;
-            } else if (e.timeStamp - scrollStart < 500) {
-                // 스크롤 시작 지점에서 500ms 이내에 휠을 한 번 더 하면 섹션 2로 이동
-                changeSection(currentSection - 1);
-            } else {
-                scrollStart = 0; // 스크롤 시작 지점 초기화
-            }
-        }
+  if (isTransitioning) return;
+
+  if (currentSection < 2) {
+    if (e.deltaY > 0 && currentSection < sections.length - 1) {
+      changeSection(currentSection + 1);
+    } else if (e.deltaY < 0 && currentSection > 0) {
+      changeSection(currentSection - 1);
     }
+  } else if (currentSection === 2) {
+    const isScrolledToTop = section3.scrollTop === 0;
+    if (e.deltaY < 0 && isScrolledToTop) {
+      if (scrollStart === 0) {
+        scrollStart = e.timeStamp;
+      } else if (e.timeStamp - scrollStart < 500) {
+        changeSection(currentSection - 1);
+      } else {
+        scrollStart = 0;
+      }
+    }
+  }
 });
 
-// 섹션 3 스크롤 이벤트를 감지하여 스크롤 시작 지점 초기화
 section3.addEventListener('scroll', () => {
-    scrollStart = 0;
+  scrollStart = 0;
 });
