eb34adb808b8f7342f88157ddfe93019703665af
leekiwon31221213 <129711547+leekiwon31221213@users.noreply.github.com>
2024-02-21 03:21:46 +0900

    [update] 24-02-21 새벽작업

--- js/common.js
+++ js/common.js
@@ -1,68 +1,3 @@
-// let currentSection = 0;
-// const sections = document.querySelectorAll('.section');
-// const section3 = document.querySelector('.section3');
-// let scrollStart = 0;
-// let isTransitioning = false;
-
-// const changeSection = (next) => {
-//   if (next >= 0 && next < sections.length && !isTransitioning) {
-//     isTransitioning = true;
-
-//     sections.forEach((section, index) => {
-//       section.style.left = index > next ? '100%' : '-100%';
-//       section.style.top = '0';
-
-//       // 모든 섹션에서 active-section 클래스 제거
-//       section.classList.remove('section2-active');
-//     });
-
-//     sections[next].style.left = '0';
-//     sections[next].style.top = '0';
-
-//     // 섹션 2가 활성화될 때만 active-section 클래스 추가
-//     if (next === 1) {
-//       sections[next].classList.add('section2-active');
-//     }
-
-//     currentSection = next;
-
-//     setTimeout(() => {
-//       isTransitioning = false;
-//     }, 500);
-//   }
-
-//   document.body.style.overflow = currentSection === 2 ? 'auto' : 'hidden';
-// };
-
-// window.addEventListener('wheel', (e) => {
-//   if (isTransitioning) return;
-
-//   if (currentSection < 2) {
-//     if (e.deltaY > 0 && currentSection < sections.length - 1) {
-//       changeSection(currentSection + 1);
-//     } else if (e.deltaY < 0 && currentSection > 0) {
-//       changeSection(currentSection - 1);
-//     }
-//   } else if (currentSection === 2) {
-//     const isScrolledToTop = section3.scrollTop === 0;
-//     if (e.deltaY < 0 && isScrolledToTop) {
-//       if (scrollStart === 0) {
-//         scrollStart = e.timeStamp;
-//       } else if (e.timeStamp - scrollStart < 500) {
-//         changeSection(currentSection - 1);
-//       } else {
-//         scrollStart = 0;
-//       }
-//     }
-//   }
-// });
-
-// section3.addEventListener('scroll', () => {
-//   scrollStart = 0;
-// });
-
-/* 테스트중 */
-
 const section3 = document.querySelector('.section3');
 if (window.innerWidth >= 1400) {
   let currentSection = 0;
@@ -128,8 +63,6 @@ if (window.innerWidth >= 1400) {
     scrollStart = 0;
   });
 }
-
-/*  */
 
 /* section3 scroll*/
 /* section3-2 */
